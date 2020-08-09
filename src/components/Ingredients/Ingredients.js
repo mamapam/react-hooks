6222eb3ceb865092327c8df5e55fa4f3eca5acfb
mamapam <mamapam.dev@gmail.com>
2020-08-08 21:10:44 -0400

    Load data with useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,27 @@ import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    // runs whenever this component gets rerendered
+    // after and for every render cycle
+    // 2nd argument is dependency and only when it changes will function rerun
+    // empty array means it will run when component first rendered and won't run there after
+    // empty array is like componentDidMount()
+    fetch('https://react-hooks-17c3c.firebaseio.com/ingredients.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch('https://react-hooks-17c3c.firebaseio.com/ingredients.json', {
